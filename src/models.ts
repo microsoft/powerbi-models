8abbf1c3b7a0ebdeec394b9c3b9848b1d9b22a68
Guy Moses <guymos@microsoft.com>
2021-07-19 14:42:31 +0000

    Merged PR 187269: Post-release undo of programmatic zoom revert
    This reinstates the changes reverted before release (while keeping the higher version number)

--- src/models.ts
+++ src/models.ts
@@ -1092,6 +1092,7 @@ export interface ISettings {
     visualRenderedEvents?: boolean;
     visualSettings?: IVisualSettings;
     localeSettings?: ILocaleSettings;
+    zoomLevel?: number;
 }
 
 export interface IReportBars {
@@ -1772,5 +1773,10 @@ export function validateCommandsSettings(input: any): IError[] {
 
 export function validateCustomTheme(input: any): IError[] {
     const errors: any[] = Validators.customThemeValidator.validate(input);
+    return errors ? errors.map(normalizeError) : undefined;
+}
+
+export function validateZoomLevel(input: any): IError[] {
+    const errors: any[] = Validators.zoomLevelValidator.validate(input);
     return errors ? errors.map(normalizeError) : undefined;
 }
