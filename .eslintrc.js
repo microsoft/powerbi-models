a734aa69296ae4a0cff989b56e8bb2f8cbc94fd4
Ali Abu Foul <aliabufoul@microsoft.com>
2022-05-29 14:03:31 +0000

    Merged PR 267928: Fix broken powerbi-models@1.10.1 - Release version 1.10.2

--- .eslintrc.js
+++ .eslintrc.js
@@ -9,7 +9,7 @@ module.exports = {
     ],
     "parser": "@typescript-eslint/parser",
     "parserOptions": {
-      "project": "tsconfig.json",
+      "project": "webpack.test.tsconfig.json",
       "sourceType": "module"
     },
     "plugins": [
