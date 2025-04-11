701f4647309204b751b506daa14dffac9ee3a9dc
Pratik Jain (Accenture International Limited) <v-jainpratik@microsoft.com>
2025-04-11 05:12:12 +0000

    Merged PR 718001: Enabling code coverage and added azurepipelines-coverage.yml for QCS requirement - powerbi-models
    Runs:-
    
    **powerbi-models-Buddy:-** https://dev.azure.com/powerbi/Embedded/_build/results?buildId=13759958&view=results
    
    #### AI description  (iteration 1)
    #### PR Classification
    New feature for enabling code coverage.
    
    #### PR Summary
    This pull request introduces code coverage reporting and integrates it with Azure Pipelines for the `powerbi-models` project.
    - `karma.conf.js`: Added `karma-typescript` and `junit` reporters, configured Puppeteer for headless Chrome, and set up coverage reporting.
    - `/.pipelines/OneBranch.Buddy.yml`: Added steps to install Puppeteer, run Karma tests, and publish test and code coverage results.
    - `package.json`: Added `karma-junit-reporter` dependency.
    - `/azurepipelines-coverage.yml`: New file to configure code coverage status and targets.
    <!-- GitOpsUserAgent=GitOps.Apps.Server.pullrequestcopilot -->
    
    Related work items: #1657921

--- karma.conf.js
+++ karma.conf.js
@@ -1,39 +1,68 @@
 var argv = require('yargs').argv;
+
+process.env.CHROME_BIN = require('puppeteer').executablePath();
+
 const flags = [
     '--disable-extensions',
     '--no-proxy-server',
     '--js-flags="--max_old_space_size=6500"',
-    '--high-dpi-support=1'
+    '--high-dpi-support=1',
+    '--no-sandbox',
+    '--headless',
+    '--disable-gpu',
+    '--window-size=800,800',
 ];
 module.exports = function (config) {
     config.set({
-        frameworks: ['jasmine'],
+        frameworks: ['jasmine', 'karma-typescript'],
         files: [
             './tmp/**/*.js'
         ],
         exclude: [],
-        reporters: argv.debug ? ['spec', 'kjhtml'] : ['spec', 'coverage', 'kjhtml'],
+        preprocessors: {
+          './src/**/*.ts': ['karma-typescript'],
+          './test/**/*.ts': ['karma-typescript']
+        },
+        reporters: argv.debug ? ['spec', 'kjhtml'] : ['spec', 'coverage', 'progress', 'karma-typescript', 'junit', 'kjhtml'],
         autoWatch: true,
-        browsers: ["Chrome_headless"],
+        browsers: ["Chrome_headless_custom"],
         plugins: [
             'karma-chrome-launcher',
             'karma-jasmine',
             'karma-spec-reporter',
+            'karma-junit-reporter',
+            'karma-jasmine-html-reporter',
             'karma-coverage',
-            'karma-jasmine-html-reporter'
+            'karma-typescript'
         ],
         customLaunchers: {
-            'Chrome_headless': {
+            'Chrome_headless_custom': {
                 base: 'Chrome',
                 flags: flags.concat("--no-sandbox", "--window-size=800,800"),
             },
         },
+        junitReporter: {
+          outputDir: 'test-results',
+          outputFile: 'testresults.xml',
+          useBrowserName: false
+        }, 
         preprocessors: { './tmp/**/*.js': ['coverage'] },
         coverageReporter: {
+            dir: 'coverage',
             reporters: [
-                { type: 'html' },
-                { type: 'text-summary' }
+                { type: 'cobertura', subdir: '.', file: 'cobertura-coverage.xml' },
+                { type: 'html', subdir: 'html' }
             ]
+        },
+        karmaTypescriptConfig: {
+          tsconfig: './tsconfig.json',
+          reports: {
+            "cobertura": {
+              "directory": "coverage",
+              "filename": "cobertura-coverage.xml"
+            },
+            "html": "coverage/html"
+          } 
         },
         retryLimit: 0,
         logLevel: argv.debug ? config.LOG_DEBUG : config.LOG_INFO,
